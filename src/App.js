3f8e10f05b6499c1b9bb64760da86024ef3b50a7
George Kiknadze <g.kiknadze@hotmail.com>
2016-10-09 00:41:17 +0200

    Add Elemental UI and setting grid system

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import './styles/styles.min.css';
+import './styles/customStyle.css';
+import { Row, Col, Card } from 'elemental';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello React</h1>
+        <Row>
+          <Col sm="1/3">
+            <Card>
+              Test
+            </Card>
+          </Col>
+          <Col sm="1/3">
+            <Card>
+              Test
+            </Card>
+          </Col>
+          <Col sm="1/3">
+            <Card>
+              Test
+            </Card>
+          </Col>
+        </Row>
       </div>
     );
   }
