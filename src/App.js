984659f7c5e192580e163601bc55a4a27ff1f098
George Kiknadze <g.kiknadze@hotmail.com>
2016-10-09 13:41:54 +0200

    Customize, added buttons

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React, { Component } from 'react';
 import './styles/styles.min.css';
 import './styles/customStyle.css';
-import { Row, Col, Card } from 'elemental';
+import { Row, Col, Card, Button } from 'elemental';
 
 class App extends Component {
   render() {
     return (
       <div>
+
         <Row>
-          <Col sm="1/3">
+          <Col sm="1/3" className="div-center">
             <Card>
-              Test
-            </Card>
-          </Col>
-          <Col sm="1/3">
-            <Card>
-              Test
-            </Card>
-          </Col>
-          <Col sm="1/3">
-            <Card>
-              Test
+              <Row>
+                <Col sm="1/3" className="center">
+                  <Button type="hollow-primary">All channels</Button>
+                </Col>
+                <Col sm="1/3" className="center">
+                  <Button type="hollow-success">Online channels</Button>
+                </Col>
+                <Col sm="1/3" className="center">
+                  <Button type="hollow-danger">Offline channels</Button>
+                </Col>
+              </Row>
             </Card>
           </Col>
         </Row>
+
       </div>
     );
   }
