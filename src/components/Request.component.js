5ca66c0c5242b06a2f6b4292dc0cc9d7b1cbfd7e
George Kiknadze <g.kiknadze@hotmail.com>
2016-10-11 23:15:22 +0200

    Remove jquery, trying to make ajax request and save res into state.

--- src/components/Request.component.js
+++ src/components/Request.component.js
@@ -3,23 +3,53 @@ import request from 'superagent';
 
 class Request extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+      channels: [
+        "ESL_SC2", "OgamingSC2",
+        "cretetion", "freecodecamp",
+        "storbeck", "habathcx",
+        "RobotCaleb", "noobs2ninjas"
+      ]
+    }
+  }
+
+  componentDidMount() {
+    this.state.channels.forEach(function(channel) {
+      request
+        .get('https://api.twitch.tv/kraken/channels/' + channel)
+        .set({'Client-ID': '8y4c38tqcnhbanpq2023ro8bcos4ykb'})
+        .end(function (err, res) {
+          this.state.items.setState({
+            items: this.state.items.push(res)
+          }).bind(Request);
+        });
+      // this.getChannel(channel);
+    });
+  }
+
+  getChannel(arg) {
+    request
+      .get('https://api.twitch.tv/kraken/channels/' + arg)
+      .set({'Client-ID': '8y4c38tqcnhbanpq2023ro8bcos4ykb'})
+      .end(function (err, res) {
+        this.setState({
+          items: this.state.items.push(res)
+        })
+      });
+  }
+
+
+
   render () {
+    console.log(this.state.items);
     return (
       <div>
-        {this.props.channels.map(function(channel) {
-          request
-            .get('https://api.twitch.tv/kraken/channels/' + channel)
-            .set({'Client-ID': '8y4c38tqcnhbanpq2023ro8bcos4ykb'})
-            .end(function (err, res) {
-              return (
-                <div>
-                  {res}
-                </div>
-              );
-            })
-        })}
+        {this.state.items}
       </div>
-    )
+    );
   }
 };
 
@@ -30,3 +60,4 @@ Request.propTypes = {
 };
 
 export default Request;
+
